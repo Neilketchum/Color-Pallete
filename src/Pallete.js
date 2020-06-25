a216e665b6488ec3f509b6f918187947d61ade28
Neilketchum <daipayanh@gmail.com>
2020-06-25 15:43:56 +0530

    Created React App and created Palletes

---
+++ src/Pallete.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+class Pallete extends Component{
+    render(){
+        return(
+            <div className = 'Pallete'>
+                    {/* Navbar */}
+                <div className="Pallete-colors">
+                    {/* Bunch of Colors */}
+                </div>
+                {/* Footer */}
+            </div>
+        )
+    }
+}
+export default Pallete;
