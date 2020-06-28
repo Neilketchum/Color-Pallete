118e19e959f83f48ce94edfbdac41d3722824ca6
Neilketchum <daipayanh@gmail.com>
2020-06-28 18:12:10 +0530

    Added Color Helper to add diffent shades

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import Pallete from './Pallete'
 import seedColors from "./seedColors"
+import {generatePalette } from './colorHelpers'
 class App extends Component{
     render(){
+        console.log(generatePalette(seedColors[4]))
         return(
             <div>
               <Pallete {...seedColors[2]}/>
