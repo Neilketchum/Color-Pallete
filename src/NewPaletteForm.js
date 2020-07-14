d028c0523bbff5ec9904f12bbecb5a652538bf8e
Neilketchum <daipayanh@gmail.com>
2020-07-14 20:50:57 +0530

    Added Draggable ColorBox

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -12,6 +12,7 @@ import MenuIcon from "@material-ui/icons/Menu";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import {ChromePicker} from 'react-color';
 import Button from '@material-ui/core/Button'
+import DraggableColorBox from "./DraggableColorBox";
 const drawerWidth = 400;
 
 const styles = theme => ({
@@ -21,7 +22,9 @@ const styles = theme => ({
   appBar: {
     transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
+      duration: theme.transitions.duration.leavingScreen,
+    
+
     })
   },
   appBarShift: {
@@ -54,13 +57,16 @@ const styles = theme => ({
     justifyContent: "flex-end"
   },
   content: {
-    flexGrow: 1,
+    flexGrow: "1",
+   
     padding: theme.spacing.unit * 3,
     transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen
     }),
-    marginLeft: -drawerWidth
+    marginLeft: -drawerWidth,
+    height: "calc(100vh - 64px)",
+
   },
   contentShift: {
     transition: theme.transitions.create("margin", {
@@ -162,11 +168,9 @@ class NewPaletteForm extends Component {
         >
           <div className={classes.drawerHeader} />
          
-        <ul>
-            {this.state.colors.map(color=>(
-                <li style = {{background:color}} >{color}</li>
+            {this.state.colors.map(col=>(
+                <DraggableColorBox color = {col}/>
             ))}
-        </ul>
 
 
 
