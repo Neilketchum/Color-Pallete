e461ad3ebc50a6e65711a587006692ead41d388f
Neilketchum <daipayanh@gmail.com>
2020-07-18 21:13:03 +0530

    Added Option to Delete Palette

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -3,6 +3,8 @@ import React from 'react'
 
 import {withStyles} from '@material-ui/styles'
 
+import DeleteIcon from '@material-ui/icons/Delete'
+
 const styles = {
     root:{
        width:"20%",
@@ -12,13 +14,48 @@ const styles = {
        position:"relative",
        cursor:"pointer",
        marginBottom:"-3.5px",
+       "&:hover svg":{
+        color:'white'
     }
+    },
+    boxContent:{
+        position: 'absolute',
+        width:'100px',
+        left:'0px',
+        bottom: '0px',
+        padding: '10px',
+        // color:   props=>
+        // chroma(props.background).luminance() >= 0.7?"rgba(0,0,0,0.8)":"white",
+        letterSpacing: '1px',
+        color:'rgba(0,0,0,0.5)',
+        textTransform: 'uppercase',
+        fontSize: '12px',
+        display:'flex',
+        justifyContent:'space-between',
+        alignItems:'center',
+        
+    },
+    deleteIcon:{
+        marginLeft:'220px',
+        // transition:'all 0.3 ease-in-out',
+        transform:'scale(1.2)',
+        color:'rgba(0,0,0,0.5)',
+        // marginBottom:
+        
+    },
+    
+
 }
 
  function DraggableColorBox(props) {
+     const {classes} = props;
     return (
-        <div style = {{background:props.color}} className = {props.classes.root}>
-            {props.name}
+        <div style = {{background:props.color}} className = {classes.root}>
+            <div className={classes.boxContent}>
+                <span>   {props.name} </span>
+                <DeleteIcon className = {classes.deleteIcon} onClick = {props.handleClick}_/>
+            </div>
+         
         </div>
     )
 }
--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -89,6 +89,12 @@ class NewPaletteForm extends Component {
     this.addNewColor = this.addNewColor.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.removeColor = this.removeColor.bind(this);
+  }
+  removeColor(colorName){
+    this.setState({
+      colors:this.state.colors.filter(color=>color.name !== colorName)
+    })
   }
   componentDidMount() {
     ValidatorForm.addValidationRule("isColorNameUnique", value =>
@@ -242,7 +248,7 @@ class NewPaletteForm extends Component {
         >
           <div className={classes.drawerHeader} />
           {this.state.colors.map(color => (
-            <DraggableColorBox color={color.color} name={color.name} />
+            <DraggableColorBox key = {color.name} color={color.color} name={color.name}  handleClick = {() => this.removeColor(color.name)}/>
           ))}
         </main>
       </div>
