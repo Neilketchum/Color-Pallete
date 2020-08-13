037cf409d9ee14fe228e7652f6ab7e0fd629c3da
Neilketchum <daipayanh@gmail.com>
2020-08-13 19:52:04 +0530

    Drag and Drop Delete

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,65 +1,49 @@
-import React from 'react'
-
-
-import {withStyles} from '@material-ui/styles'
-
-import {SortableElement} from 'react-sortable-hoc';
-
-import DeleteIcon from '@material-ui/icons/Delete'
+import React from "react";
+import { SortableElement } from "react-sortable-hoc";
+import { withStyles } from "@material-ui/styles";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 const styles = {
-    root:{
-       width:"25%",
-       height:"25%",
-       margin:"10 auto",
-       display:"inline-block",
-       position:"relative",
-       cursor:"pointer",
-       border:'2px solid black',
-       marginBottom:"-3.5px",
-       "&:hover svg":{
-        color:'white'
+  root: {
+    width: "20%",
+    height: "25%",
+    margin: "0 auto",
+    display: "inline-block",
+    position: "relative",
+    cursor: "pointer",
+    marginBottom: "-3.5px",
+    "&:hover svg": {
+      color: "white",
+      transform: "scale(1.5)"
     }
-    },
-    boxContent:{
-        position: 'absolute',
-        width:'100px',
-        left:'0px',
-        bottom: '0px',
-        padding: '10px',
-        // color:   props=>
-        // chroma(props.background).luminance() >= 0.7?"rgba(0,0,0,0.8)":"white",
-        letterSpacing: '1px',
-        color:'rgba(0,0,0,0.5)',
-        textTransform: 'uppercase',
-        fontSize: '12px',
-        display:'flex',
-        justifyContent:'space-between',
-        alignItems:'center',
-        
-    },
-    deleteIcon:{
-        marginLeft:'220px',
-        // transition:'all 0.3 ease-in-out',
-        transform:'scale(1.2)',
-        color:'rgba(0,0,0,0.5)',
-        // marginBottom:
-        
-    },
-    
+  },
+  boxContent: {
+    position: "absolute",
+    width: "100%",
+    left: "0px",
+    bottom: "0px",
+    padding: "10px",
+    color: "rgba(0, 0, 0, 0.5)",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px",
+    display: "flex",
+    justifyContent: "space-between"
+  },
+  deleteIcon: {
+    transition: "all 0.3s ease-in-out"
+  }
+};
 
-}
-const DraggableColorBox = SortableElement((props) =>{
-     const {classes} = props;
-     console.log(props.color)
-    return (
-        <div  className = {classes.root} style = {{backgroundColor:props.color}} >
-            <div className={classes.boxContent}>
-                <span>   {props.name} </span>
-                <DeleteIcon className = {classes.deleteIcon} onClick = {props.handleClick}/>
-            </div>
-         
-        </div>
-    )
-})
-export default withStyles(styles)(DraggableColorBox)
+const DraggableColorBox = SortableElement(props => {
+  const { classes, handleClick, name, color } = props;
+  return (
+    <div className={classes.root} style={{ backgroundColor: color }}>
+      <div className={classes.boxContent}>
+        <span> {name}</span>
+        <DeleteIcon className={classes.deleteIcon} onClick={handleClick} />
+      </div>
+    </div>
+  );
+});
+export default withStyles(styles)(DraggableColorBox);
